050dbf6cbb6844d5c466689c4d34556e6d07a21c
Félix Hernández Hernández <felix_hdezz95@hotmail.com>
2018-03-15 15:07:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 /* Init file */
 var root = document.body
+var count = 0
 
-m.render(root, [
-    m("main", [
-        m("h1", {class: "title"}, "My first app"),
-        m("button", "A button"),
-    ])
-])
+var Hello = {
+    view: function() {
+        return m("main", [
+            m("h1", {
+                class: "title"
+            }, "My first app"),
+            m("button", {
+                onclick: function() {count++}
+            }, count + " clicks"),
+        ])
+    }
+}
+
+var Splash = {
+    view: function() {
+        return m("a", {
+            href: "#!/hello"
+        }, "Enter!")
+    }
+}
+
+m.route(root, "/splash", {
+    "/splash": Splash,
+    "/hello": Hello,
+})
